c9b5cfd6c3e6f573b53f76bbd369dd639beb8024
dakotas17 <dakotas17@nycstudents.net>
2023-10-13 09:11:01 -0400

    dom

---
+++ dom.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#form");
+console.log(form)
+
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+    //grab the text input
+    //grab ALL the h2s
+    h2s: document.querySelectorAll("h2"),
+};
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
+});
